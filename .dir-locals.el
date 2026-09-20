dbda8eb37439e2d8a127779fb792e13b54f2331b
Yury Benesh <yurybdev@gmail.com>
2026-09-20 22:03:20 +0300

    dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,9 @@
-((nil . ((compile-command . "ninja -C _build"))))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+;;; Syntax: ((MODE . ((VAR . VALUE) ...)) ...)
+;;; A MODE of `nil' applies the settings to all buffers in the directory.
+
+((nil . ((eval . (let ((root (locate-dominating-file
+                                  default-directory ".dir-locals.el")))
+                       (setq-local compile-command
+                                   (format "ninja -C %s_build" root)))))))
